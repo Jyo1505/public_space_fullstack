6a7c980b5102dd496056c4c10ac4a6becd229dab
Jyotsna <merejyotsna@gmail.com>
2026-02-02 22:30:27 +0530

    config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql2");
+
 const {
   DB_HOST,
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
+  DB_PORT
 } = require("./config");
 
 const pool = mysql.createPool({
@@ -11,8 +13,23 @@ const pool = mysql.createPool({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  port: Number(DB_PORT),
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
 }).promise();
 
 module.exports = pool;
+
+
+pool.getConnection()
+  .then(conn => {
+    console.log("✅ Database connected successfully");
+    conn.release();
+  })
+  .catch(err => {
+    console.error("❌ Database connection failed:", err.message);
+  });
